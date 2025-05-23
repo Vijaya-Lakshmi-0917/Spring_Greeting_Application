5f8c8712fb2ed7dff98da408c5f0b22863581e12
S Vijaya Lakshmi <vijayalakshmi.svl.0017@gmail.com>
2025-05-23 15:09:41 +0530

    added UC3

--- src/main/java/Spring/example/SpringGreetingApplication/controller/GreetingController.java
+++ src/main/java/Spring/example/SpringGreetingApplication/controller/GreetingController.java
@@ -1,5 +1,6 @@
 package Spring.example.SpringGreetingApplication.controller;
 
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Service;
@@ -9,40 +10,47 @@ import org.springframework.beans.factory.annotation.Autowired;
 @RequestMapping("/greeting")
 public class GreetingController {
 
+    private final GreetingService greetingService;
+
     @Autowired
-    private GreetingService greetingService;
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
 
     @GetMapping
-    public ResponseEntity<Greeting> getGreeting() {
-        return greetingService.getGreetingMessage();
+    public ResponseEntity<Greeting> getGreeting(
+            @RequestParam(required = false) String firstName,
+            @RequestParam(required = false) String lastName) {
+        String message = greetingService.createGreetingMessage(firstName, lastName);
+        return ResponseEntity.ok(new Greeting(message));
     }
 
     @PostMapping
     public ResponseEntity<Greeting> postGreeting() {
-        return greetingService.getGreetingMessage();
+        return greetingService.getDefaultGreeting();
     }
 
     @PutMapping
     public ResponseEntity<Greeting> putGreeting() {
-        return greetingService.getGreetingMessage();
+        return greetingService.getDefaultGreeting();
     }
 
     @DeleteMapping
     public ResponseEntity<Greeting> deleteGreeting() {
-        return greetingService.getGreetingMessage();
+        return greetingService.getDefaultGreeting();
     }
 
-    static class Greeting {
+    public static class Greeting {
         private String message;
+
+        public Greeting() {}
 
         public Greeting(String message) {
             this.message = message;
         }
-
         public String getMessage() {
             return message;
         }
-
         public void setMessage(String message) {
             this.message = message;
         }
@@ -51,9 +59,22 @@ public class GreetingController {
 
 @Service
 class GreetingService {
-    public ResponseEntity<GreetingController.Greeting> getGreetingMessage() {
-        GreetingController.Greeting greeting = new GreetingController.Greeting("Hello World");
-        return ResponseEntity.ok(greeting);
+
+    public String createGreetingMessage(String firstName, String lastName) {
+        if (firstName != null && !firstName.isEmpty() && lastName != null && !lastName.isEmpty()) {
+            return "Hello " + firstName + " " + lastName + "!";
+        }
+        if (firstName != null && !firstName.isEmpty()) {
+            return "Hello " + firstName + "!";
+        }
+        if (lastName != null && !lastName.isEmpty()) {
+            return "Hello " + lastName + "!";
+        }
+        return "Hello World!";
+    }
+
+    public ResponseEntity<GreetingController.Greeting> getDefaultGreeting() {
+        return ResponseEntity.ok(new GreetingController.Greeting("Hello World"));
     }
 }
 
